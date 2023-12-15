2f033d59ec00613fbbb32f5116538dd0cf999ada
Md. Nasir Uddin <md.nasiruddin4067@gmail.com>
2023-12-15 13:25:06 +0600

    [DONE] contact page design improve

--- src/Component/ContactUs/ContactUs.jsx
+++ src/Component/ContactUs/ContactUs.jsx
@@ -4,20 +4,20 @@ import Navigation from "../Home/Navigation/Navigation";
 import "./ContactUs.css";
 
 const ContactUs = () => {
-    return (
-        <div className="md: container md: mx-auto">
-            <Navigation/> 
-            <div className="contact-us-filed">
-                <h1 className="text-3xl text-center pt-10">How can we help you ?</h1>
-                <div className="mt-20 text-center">
-                    <Contact/>
-                </div>
-                <div className="mt-20 text-center ">
-                    <FooterCopyRight/>
-                </div>
-            </div>
+  return (
+    <div className="md: container md: mx-auto">
+      <Navigation />
+      <div className="contact-us-filed">
+        <h1 className="text-3xl text-center pt-10">How can we help you ?</h1>
+        <div className="mt-20 w-4/5 mx-auto my-auto">
+          <Contact />
         </div>
-    );
+        <div className="mt-20 text-center ">
+          <FooterCopyRight />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactUs;
